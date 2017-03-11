17cb78ac022685b9a8ba00973557b4fdf1efbe6b
RandomByte <r@ndombyte.net>
2017-03-11 19:16:34 +0100

    Publish state to an MQTT broker instead of beeping the console

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const ping = require("ping");
-const beep = require("beepbeep");
-const host = "google.com";
+const mqtt = require("mqtt");
+const config = require("./config.json");
 const deadThreshold = 20;
 const isAliveTimespan = 300000;
 let deadCount = 0;
 let aliveCount = 0;
 let looksAliveTimestamp = 0;
+
+/* Check config */
+if (!config.host || !config.brokerUrl || !config.overallTopic || !config.detailTopic) {
+	console.log("There's something missing in your config.json, please refer to config.example.json for an example");
+	process.exit(1);
+}
+
+const mqttClient = mqtt.connect(config.brokerUrl);
+
+console.log("MQTT Broker: " + config.brokerUrl);
+console.log("Host: " + config.host);
 
 function probeHost(hostname) {
 	return ping.promise.probe(hostname).then(function(res) {
@@ -16,11 +27,12 @@ function probeHost(hostname) {
 }
 
 function probe() {
-	probeHost(host).then(function(alive) {
+	probeHost(config.host).then(function(alive) {
 		if (!alive) {
 			if (deadCount < deadThreshold) {
 				deadCount++;
 			} else {
+				publishOverallState("Offline");
 				deadCount = 0;
 				aliveCount = 0;
 				looksAliveTimestamp = 0;
@@ -28,9 +40,14 @@ function probe() {
 		} else {
 			aliveCount++;
 		}
-		console.log(`Alive count: ${aliveCount} | Dead count buffer: ${deadCount}/${deadThreshold}`);
+		let aliveCountState = `Alive count: ${aliveCount} | Dead count buffer: ${deadCount}/${deadThreshold}`;
+		console.log(aliveCountState);
+		publishDetailState(aliveCountState);
+
 		if (aliveCount > 100) {
-			console.log("======== Looks alive ========");
+			let appearsOnlineState = "Appears online";
+			console.log(appearsOnlineState);
+			publishOverallState(appearsOnlineState);
 			if (!looksAliveTimestamp) {
 				console.log("Setting timestamp");
 				looksAliveTimestamp = new Date().getTime();
@@ -41,14 +58,32 @@ function probe() {
 			let diff = timestamp - looksAliveTimestamp;
 
 			if (diff > isAliveTimespan) {
-				console.log(`======== Is alive (${diff / 1000 / 60}min passed without reset)========`);
-				beep(1000);
+				let onlineState = `Is online: ${diff / 1000 / 60}min passed without reset`;
+				console.log(onlineState);
+				publishDetailState(onlineState);
+				publishOverallState("Online");
 			} else {
-				console.log(`Assurance in ${(isAliveTimespan - diff) / 1000}sec...`);
+				let assuranceState = `Appears online: assurance in ${(isAliveTimespan - diff) / 1000}sec...`;
+				console.log(assuranceState);
+				publishDetailState(assuranceState);
 			}
 		}
 		setTimeout(probe, 100);
 	});
 }
 
-probe();
+function publishOverallState(state) {
+	mqttClient.publish(config.overallTopic, state, {
+		qos: 2 // must arrive and must arrive exactly once - also ensures order
+	});
+}
+
+function publishDetailState(state) {
+	mqttClient.publish(config.detailTopic, state, {
+		qos: 2 // must arrive and must arrive exactly once - also ensures order
+	});
+}
+
+mqttClient.on("connect", function() {
+	probe();
+});
